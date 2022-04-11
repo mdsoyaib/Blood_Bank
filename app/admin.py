709fa902ff3145bd9a4e2b1032057efa46a6fc25
Soyaib <mdsoyaib2025@gmail.com>
2022-04-11 13:13:29 +0600

    modifying admin panel

--- app/admin.py
+++ app/admin.py
@@ -13,7 +13,12 @@ admin.site.register(Feedback)
 @admin.register(FeedbackLike)
 class FeedbackLikeAdmin(admin.ModelAdmin):
     list_display = ("user", "feedback")
+    list_per_page = 10
 
 admin.site.register(ContactForm)
 admin.site.register(BloodRequest)
-admin.site.register(RequestToDonor)
+# admin.site.register(RequestToDonor)
+@admin.register(RequestToDonor)
+class RequestToDonorAdmin(admin.ModelAdmin):
+    list_display = ("id", "from_patient", "to_donor", "blood", "message", "created_at")
+    list_per_page = 10
